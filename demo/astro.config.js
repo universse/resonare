d0c17cc16b48899031048db897f2dd30f1900c33
universse <phuoc317049@gmail.com>
2025-07-29 01:41:54 +0800

    chore: deploy

--- demo/astro.config.js
+++ demo/astro.config.js
@@ -5,4 +5,13 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
 	adapter: cloudflare(),
 	integrations: [react()],
+	vite: {
+		resolve: {
+			alias: {
+				...(process.env.NODE_ENV === "production" && {
+					"react-dom/server": "react-dom/server.edge",
+				}),
+			},
+		},
+	},
 });
